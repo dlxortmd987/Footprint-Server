71727105e0edb020191603695034646fc790fd34
dlxortmd987 <dlxortmd987@gmail.com>
2022-05-19 01:53:35 +0900

    improve: repository interface 메소드 추가

--- footprint/src/main/java/com/umc/footprint/src/repository/WalkRepository.java
+++ footprint/src/main/java/com/umc/footprint/src/repository/WalkRepository.java
@@ -1,7 +1,30 @@
 package com.umc.footprint.src.repository;
 
 import com.umc.footprint.src.model.Walk;
+import com.umc.footprint.src.walks.model.ObtainedBadgeInterface;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface WalkRepository extends JpaRepository<Walk, Integer> {
+    @Query(value = "SELECT \n" +
+            "       CASE\n" +
+            "            WHEN (sum(Walk.distance) between 10 and 30) then 2\n" +
+            "            when (sum(Walk.distance) between 30 and 50) then 3\n" +
+            "            WHEN (sum(Walk.distance) between 50 and 100) then 4\n" +
+            "            WHEN (sum(Walk.distance) > 100) then 5\n" +
+            "        else 0\n" +
+            "        end as distanceBadgeIdx,\n" +
+            "       CASE\n" +
+            "            when (count(Walk.walkIdx) = 1) then 1" +
+            "            when (count(Walk.walkIdx) between 10 and 19) then 6\n" +
+            "            when (count(Walk.walkIdx) between 20 and 29) then 7\n" +
+            "            when (count(Walk.walkIdx) >= 30) then 8\n" +
+            "        else 0\n" +
+            "        end as recordBadgeIdx\n" +
+            "From Walk\n" +
+            "Where userIdx = ?1\n and status = 'ACTIVE'" +
+            "group by Walk.userIdx", nativeQuery = true)
+    ObtainedBadgeInterface getAcquiredBadgeIdxList(int userIdx);
+
+    boolean existsByUserIdx(Integer userIdx);
 }
