98092b4cee60979b6ad3575a79cb7039f44ee8c3
dlxortmd987 <dlxortmd987@gmail.com>
2022-05-19 01:52:41 +0900

    improve: dto, vo 변경

--- footprint/src/main/java/com/umc/footprint/src/walks/model/SaveWalk.java
+++ footprint/src/main/java/com/umc/footprint/src/walks/model/SaveWalk.java
@@ -25,11 +25,11 @@ public class SaveWalk {
     @ApiModelProperty(example = "산책 거리")
     private double distance;
 
-    @ApiModelProperty(example = "산책 좌표")
-    private List<List<Double>> coordinates;
-
     @ApiModelProperty(example = "산책한 유저 인덱스")
     private int userIdx;
+
+    @ApiModelProperty(example = "산책 좌표")
+    private List<List<Double>> coordinates;
 
     @ApiModelProperty(example = "산책 string 좌표", hidden = true)
     private String strCoordinates;
@@ -40,11 +40,10 @@ public class SaveWalk {
     @ApiModelProperty(example = "소비한 칼로리")
     private int calorie;
 
-    @ApiModelProperty(example = "각 발자국 기록의 사진 개수")
-    private List<Integer> photoMatchNumList;
+    private String thumbnail;
 
     @Builder
-    public SaveWalk(int walkIdx, LocalDateTime startAt, LocalDateTime endAt, double distance, List<List<Double>> coordinates, int userIdx, String strCoordinates, Double goalRate, int calorie, List<Integer> photoMatchNumList) {
+    public SaveWalk(int walkIdx, LocalDateTime startAt, LocalDateTime endAt, double distance, List<List<Double>> coordinates, int userIdx, String strCoordinates, Double goalRate, int calorie, String thumbnail) {
         this.walkIdx = walkIdx;
         this.startAt = startAt;
         this.endAt = endAt;
@@ -54,7 +53,7 @@ public class SaveWalk {
         this.strCoordinates = strCoordinates;
         this.goalRate = goalRate;
         this.calorie = calorie;
-        this.photoMatchNumList = photoMatchNumList;
+        this.thumbnail = thumbnail;
     }
 
     public void setUserIdx(int userIdx) {
