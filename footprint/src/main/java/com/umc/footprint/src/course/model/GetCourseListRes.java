abdeefc6cc5385f4a6d7ca0ffec91c96011ac8c8
minaver <psmjoshua@gmail.com>
2022-08-28 17:10:04 +0900

    improve:[추가] GetCourseListRes DTO List CourseDist 기준으로 오름차순 정렬

--- footprint/src/main/java/com/umc/footprint/src/course/model/GetCourseListRes.java
+++ footprint/src/main/java/com/umc/footprint/src/course/model/GetCourseListRes.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 @Getter
 @NoArgsConstructor
-public class GetCourseListRes {
+public class GetCourseListRes implements Comparable<GetCourseListRes>{
     private int courseIdx;
     private double startLat;
     private double startLong;
@@ -33,5 +33,15 @@ public class GetCourseListRes {
         this.courseTags = courseTags;
         this.courseImg = courseImg;
         this.userCourseMark = userCourseMark;
+    }
+
+    @Override
+    public int compareTo(GetCourseListRes getCourseListRes){
+        if(getCourseListRes.courseDist < courseDist){
+            return 1;
+        } else if(getCourseListRes.courseDist > courseDist){
+            return -1;
+        }
+        return 0;
     }
 }
