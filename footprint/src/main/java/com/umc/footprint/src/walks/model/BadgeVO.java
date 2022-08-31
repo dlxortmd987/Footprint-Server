3771e6a7bbd7ed9015202b00f30741010c137901
dlxortmd987 <dlxortmd987@gmail.com>
2022-08-31 13:31:34 +0900

    [fix]: 산책 저장 API Respose에 저장된 walkIdx 추가

---
+++ footprint/src/main/java/com/umc/footprint/src/walks/model/BadgeVO.java
@@ -0,0 +1,30 @@
+package com.umc.footprint.src.walks.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor
+public class BadgeVO {
+    // 뱃지 이름, 뱃지 url
+    @ApiModelProperty(example = "뱃지 인덱스")
+    private int badgeIdx;
+
+    @ApiModelProperty(example = "뱃지 이름")
+    private String badgeName;
+
+    @ApiModelProperty(example = "뱃지 사진 Url")
+    private String badgeUrl;
+
+    @Builder
+    public BadgeVO(int badgeIdx, String badgeName, String badgeUrl) {
+        this.badgeIdx = badgeIdx;
+        this.badgeName = badgeName;
+        this.badgeUrl = badgeUrl;
+    }
+}
+
+
